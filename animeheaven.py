061290c5b7a20ec6676cedc2f35c6d8fbd9babc1
Ondřej Janošík <j.ondra14@gmail.com>
2018-09-07 22:52:07 +0200

    Updated the browser header, just for fun

--- animeheaven.py
+++ animeheaven.py
@@ -22,7 +22,7 @@ import tqdm
 
 session = requests.Session()
 headers = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0'
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
 }
 
 
